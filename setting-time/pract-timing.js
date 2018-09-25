ce1063e3fa28245728810f9f97702afa4bc45b69
George Colon <geocolon@gmail.com>
2018-09-24 20:29:01 -0700

    Updating

--- setting-time/pract-timing.js
+++ setting-time/pract-timing.js
@@ -2,18 +2,18 @@
 
 const countFive = () => {
   let i;
-    
+  
   function doSetTimeout(i) {
     setTimeout(() => {
+        
       console.log('Wait one sec...', i);
-    }, 5000); 
+    }, i * 1000); 
   }
-    
+  
   for(i = 1; i <= 5; i++){
     doSetTimeout(i);
   }
-  
+
 };
-  
+
 countFive();
-  
